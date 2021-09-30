278d4615bc2d8039d499f002d42302c6997198a4
Garrison-Shoemake <69227891+Garrison-Shoemake@users.noreply.github.com>
2021-09-30 15:13:24 -0500

    added pause menu toggling

---
+++ 0x00-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseCanvas;
+    private bool isPaused = false;
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            { 
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        pauseCanvas.SetActive(true);
+        Time.timeScale = 0f;
+        
+    }
+    public void Resume()
+    {
+        isPaused = false;
+        pauseCanvas.SetActive(false);
+        Time.timeScale = 1f;
+        
+    }
+}
